21db46e2d728666474488b9c003e17a43b602fc9
Cris R <cristofer@gmail.com>
2020-05-22 22:50:45 +0200

    Fix test, return hello there!

--- lita-hello/lib/lita/handlers/hello.rb
+++ lita-hello/lib/lita/handlers/hello.rb
@@ -3,6 +3,10 @@ module Lita
     class Hello < Handler
       route /^hi/, :greeting
 
+      def greeting(response)
+        response.reply("hello there!")
+      end
+
       Lita.register_handler(self)
     end
   end
