2ef9690e5b79bf2771e542b655c580704747c248
Cris R <cristofer@gmail.com>
2020-05-22 23:03:39 +0200

    Add route to unknown command

--- lita-hello/lib/lita/handlers/hello.rb
+++ lita-hello/lib/lita/handlers/hello.rb
@@ -1,10 +1,15 @@
 module Lita
   module Handlers
     class Hello < Handler
+      route /(^.*?)/, :unknown
       route /^hi/, :greeting
 
       def greeting(response)
         response.reply("hello there!")
+      end
+
+      def unknown(response)
+        response.reply("Sorry, I do not know that")
       end
 
       Lita.register_handler(self)
--- lita-hello/spec/lita/handlers/hello_spec.rb
+++ lita-hello/spec/lita/handlers/hello_spec.rb
@@ -3,8 +3,13 @@ require "spec_helper"
 describe Lita::Handlers::Hello, lita_handler: true do
   it { is_expected.to route("hi").to(:greeting) }
 
-  it "responds with hello there!" do
+  it "responds with hello there! when receives hi" do
     send_command("hi")
     expect(replies.last).to eq("hello there!")
+  end
+
+  it "responds empty when is another command different than hi" do
+    send_command("bye")
+    expect(replies.last).to eq("Sorry, I do not know that")
   end
 end
