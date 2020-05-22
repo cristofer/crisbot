220fac1cd4299d8cb153775719c8c892600b2e87
Cris R <cristofer@gmail.com>
2020-05-22 22:37:58 +0200

    Pump bundle version to ~ 2.0. Write first test

--- lita-hello/spec/lita/handlers/hello_spec.rb
+++ lita-hello/spec/lita/handlers/hello_spec.rb
@@ -1,4 +1,8 @@
 require "spec_helper"
 
 describe Lita::Handlers::Hello, lita_handler: true do
+  it "responds with hello there!" do
+    send_command("hi")
+    expect(replies.last).to eq("hello there!")
+  end
 end
